bd831273d69cb4f259e11667fa1e9928f3bbe212
Jaime <jaime251102@gmail.com>
2022-03-10 14:05:52 -0400

    first commit

---
+++ blazor-crud/Data/CrudDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blazor_crud.Data;
+
+internal sealed class CrudDbContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=DbDealer.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Post[] postsToSeed = new Post[6];
+
+        for (int i = 1; i <= 6; i++)
+        {
+            postsToSeed[i - 1] = new Post
+            {
+                Id = i,
+                Chasis = $"Chasis {i}",
+                Marca = $"Marca {i}",
+                Modelo = $"Modelo {i}",
+                Color = $"Color {i}",
+                Year = $"Year {i}",
+                Comentario = $"Comentario {i}",
+                Foto = $"Foto {i}",
+            };
+        }
+
+        modelBuilder.Entity<Post>().HasData(postsToSeed);
+
+    }
+
+}
+
---
+++ blazor-crud/Data/Post.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor_crud.Data;
+
+internal sealed class Post
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Chasis { get; set; } = string.Empty;
+
+    [Required]
+    public string Marca { get; set; } = string.Empty;
+
+    [Required]
+    public string Modelo { get; set; } = string.Empty;
+
+    [Required]
+    public string Color { get; set; } = string.Empty;
+
+    [Required]
+    public string Year { get; set; } = string.Empty;
+
+    [Required]
+    public string Comentario { get; set; } = string.Empty;
+
+    [Required]
+    public string Foto { get; set; } = string.Empty;
+}
+
